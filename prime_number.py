fd57dd6881b1521c6937ed5fa6bdcbb951dba88a
YukiSATO1217 <20041217yuki@gmail.com>
2023-12-01 16:53:07 +0900

    制御構文

--- prime_number.py
+++ prime_number.py
@@ -2,3 +2,17 @@ a = input("aの値を入力: ")
 b = input("bの値を入力: ")
 
 # TODO
+a = int(a)
+b = int(b)
+def pri(a):
+    if a == 1:
+        print("素数ではありません")
+    else:
+        for i in range(2, a):
+            if a % i == 0:
+                print("素数ではありません")
+                break
+        else:
+            print("素数です")
+pri(a)
+pri(b)

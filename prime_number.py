5dc4df110e73df83c7d8a5b7c50e1624d493bad0
YukiSATO1217 <20041217yuki@gmail.com>
2023-12-11 12:08:23 +0900

    修正しました

--- prime_number.py
+++ prime_number.py
@@ -1,16 +1,21 @@
-a = int(input("aの値を入力: "))
-b = int(input("bの値を入力: "))
+a = input("aの値を入力: ")
+b = input("bの値を入力: ")
 
 # TODO
 def pri(a):
-    if a == 1:
-        print("素数ではありません")
-    else:
-        for i in range(2, a):
-            if a % i == 0:
-                print("素数ではありません")
-                break
+    try:
+        a = int(a)
+        if a <= 0:
+            return "エラー"
+        elif a == 1:
+            return False
         else:
-            print("素数です")
-pri(a)
-pri(b)
+            for i in range(2, a):
+                if a % i == 0:
+                    return False
+            else:
+                return True
+    except ValueError:
+        return "エラー"
+print(pri(a))
+print(pri(b))
--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -1,16 +1,19 @@
-from math import sin
+from math import sin, pi, exp
 # --example--
 # print(sin(0))
 # >>> 0
 # -----------
-from math import pi
+def ti(f, a = 0, b =1, N = 100):    
+    h = (b - a) / N
+    S = 0
+    for k in range(1, N + 1):
+        S += (h / 2) * (f(a + (k - 1) * h) + f(a + k * h))
+    return S
 
-a = 0
-b = pi / 2
-N = 100
-h = (b - a) / N
-S=0
-
-for k in range(1,N + 1):
-    S += (h / 2) * (sin(a + (k - 1) * h) + sin(a + k * h))
-print(S)
+print(ti(sin, 0, pi / 2, 50))
+def f2(x):
+    return 4 / (1 + x ** 2)
+print(ti(f2, 0, 1, 100))
+def f3(x):
+    return (pi ** (1 / 2)) * exp(-x ** 2)
+print(ti(f3, -100, 100, 1000))

e17f48e0d451e716eb643417d6b547ec266c4104
YukiSATO1217 <20041217yuki@gmail.com>
2023-12-11 12:08:23 +0900

    関数

--- euclid.py
+++ euclid.py
@@ -6,4 +6,24 @@ while b != 0:
     a, b = b, a % b
 c = a
 
-print("最大公約数は" + str(c) +"です")
+print("最大公約数は" + str(c) + "です")
+if c == 1:
+    print (True)
+else:  
+    print(False)
+
+import random
+
+p = 0
+for i in range(100000):
+    a, b = random.sample(range(1, 10000), 2)
+    while b != 0:
+        a, b = b, a % b
+    c = a
+    if c == 1:
+        p += 1
+print("各組が互いに素である確率は" + str(p / 100000) + "です")
+
+from math import pi
+
+print(6 / (pi ** 2))
--- prime_number.py
+++ prime_number.py
@@ -1,16 +1,22 @@
-a = int(input("aの値を入力: "))
-b = int(input("bの値を入力: "))
+a = input("aの値を入力: ")
+b = input("bの値を入力: ")
 
 # TODO
 def pri(a):
-    if a == 1:
-        print("素数ではありません")
+    if "." in a:
+        print("エラー")
     else:
-        for i in range(2, a):
-            if a % i == 0:
-                print("素数ではありません")
-                break
+        a = int(a)
+        if a <= 0:
+            print("エラー")
+        elif a == 1:
+            print("素数ではありません")
         else:
-            print("素数です")
+            for i in range(2, a):
+                if a % i == 0:
+                    print("素数ではありません")
+                    break
+            else:
+                print("素数です")
 pri(a)
 pri(b)
